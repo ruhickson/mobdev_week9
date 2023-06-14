7860208c1256af7001f1fd8d148e8fcce8a41f85
Ru Hickson <ru.hickson@gmail.com>
2023-06-14 19:00:04 +0100

    Zoom Crash 2

---
+++ mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+selector: 'app-film-details',
+templateUrl: './film-details.page.html',
+styleUrls: ['./film-details.page.scss'],   
+})
+
+export class FilmDetailsPage implements OnInit {
+
+  filmId: any;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.filmId = this.activatedRoute.snapshot.paramMap.get('id')
+	}
+
+ }
---
+++ mobdev-ionic-intro/src/app/pages/films/films.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+
+@Component({
+selector: 'app-films',
+templateUrl: './films.page.html',
+styleUrls: ['./films.page.scss'],   
+})
+export class FilmsPage implements OnInit {
+
+
+constructor(private navController: NavController, private router: Router) { }
+
+
+	ngOnInit() {  
+	}
+
+
+openDetails() {
+// Both of these would work!
+// But the standard Router is recommended.
+// this.navController.navigateForward(`/tabs/films/42`);
+this.router.navigateByUrl(`/tabs/films/42`);  
+}
+
+
+goToPlanets() {
+this.navController.navigateRoot(`/tabs/planets`) 
+	}
+
+
+}
